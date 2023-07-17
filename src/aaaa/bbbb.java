3dea4ee6a929852993e80295ef765d7237ded163
seina <newskoihosi@yahoo.co.jp>
2023-07-17 21:34:14 +0900

    123123

--- src/aaaa/bbbb.java
+++ src/aaaa/bbbb.java
@@ -1,6 +1,18 @@
 package aaaa;
 
 public class bbbb {
-	//asdfasdfasdfas
+	public static void main(String[] args) {
+		System.out.println("dsfsdf");
+		System.out.println();
+		System.out.println(11);
+		System.out.println(22);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(2);
+		System.out.println(123);
+		System.out.println();
+		System.out.println();
+
+	}
 
 }
